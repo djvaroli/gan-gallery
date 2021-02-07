c2955e079154ca6e88e4099084712fff87ad0343
Daniel Varoli <daniel.varoli@gmail.com>
2021-02-07 12:09:25 -0500

    tweaked generator parameters

--- generator.py
+++ generator.py
@@ -4,38 +4,34 @@ from tensorflow.keras import layers as layers
 
 def GeneratorModel(
     input_dim, 
-    num_channels
+    num_channels,
+    batch_norm_momentum=0.9,
+    alpha=0.2,
+    start_deconv_dim=512,
+    start_dense_dim=2
 ) -> tf.keras.Model:
+    
     input = layers.Input(shape=(input_dim, ))
-    x = layers.Dense(8*8*256, use_bias=False)(input)
-    x = layers.BatchNormalization()(x)
-    x = layers.LeakyReLU()(x)
-
-    # reshape
-    x = layers.Reshape((8, 8, 256))(x)
-    x = layers.Conv2DTranspose(256, (5,5), strides=(2,2), padding="same", use_bias=False)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.LeakyReLU()(x)
+    x = layers.Dense(start_dense_dim*start_dense_dim*1024, use_bias=False)(input)
+    x = layers.BatchNormalization(momentum=batch_norm_momentum)(x)
+    x = layers.LeakyReLU(alpha=alpha)(x)
+    x = layers.Reshape((start_dense_dim, start_dense_dim, 1024))(x)
     
-    x = layers.Conv2DTranspose(128, (5,5), strides=(2,2), padding="same", use_bias=False)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.LeakyReLU()(x)
-    
-    x = layers.Conv2DTranspose(64, (5,5), strides=(2,2), padding="same", use_bias=False)(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.LeakyReLU()(x)
+    num_deconv_layers = np.log2(512 // start_dense_dim * 1.).astype(np.int32) - 1
 
-    x = layers.Conv2DTranspose(32, (5,5), strides=(2,2), padding="same", use_bias=False, activation="sigmoid")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.LeakyReLU()(x)
-
-    x = layers.Conv2DTranspose(16, (5,5), strides=(2,2), padding="same", use_bias=False, activation="sigmoid")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.LeakyReLU()(x)
-
-    x = layers.Conv2DTranspose(num_channels, (5,5), strides=(2,2), padding="same", use_bias=False, activation="sigmoid")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.LeakyReLU()(x)
+    dim = start_deconv_dim
+    for i in range(1, num_deconv_layers + 1):
+        conv_2d_transpose_layer = layers.Conv2DTranspose(
+            dim, (5,5), strides=(2,2), padding='same', use_bias=False, kernel_initializer=GlorotNormal, activation="tanh"
+        )
+        x = conv_2d_transpose_layer(x)
+        x = layers.BatchNormalization(momentum=batch_norm_momentum)(x)
+        x = layers.LeakyReLU(alpha=alpha)(x)
+        dim = dim // 2
+        
+    x = layers.Conv2DTranspose(num_channels, (5,5), strides=(2,2), padding="same", use_bias=False, activation="tanh", kernel_initializer=GlorotNormal)(x)
+    x = layers.BatchNormalization(momentum=batch_norm_momentum)(x)
+    x = layers.LeakyReLU(alpha=alpha)(x)
 
     model = tf.keras.Model(input, x)
     return model
